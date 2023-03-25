686acfa9f8a3ce413559187366e3ae4074055913
Aleksandr Shchilkin <crazyredkitten@icloud.com>
2023-03-25 11:34:54 +0200

    Implement server side data fetching for tasks

--- apps/nextjs-app/app/(app)/app/page.tsx
+++ apps/nextjs-app/app/(app)/app/page.tsx
@@ -1,9 +1,20 @@
-import UserGreetings from '@/components/UserGreetings';
-import React, { Suspense } from 'react';
-import FloatingActionButton from '@/components/FloatingActionButton';
-import TaskList from '@/components/TaskList';
+import UserGreetings from '@/components/UserGreetings'
+import React, { Suspense } from 'react'
+import { db } from '@/utils/db'
+import { getUserFromCookie } from '@/utils/auth'
+import { cookies } from 'next/headers'
+import AppClientSide from '@/components/AppClientSide'
+import { User } from '@prisma/client'
 
 const App = async () => {
+  const user = (await getUserFromCookie(cookies())) as User
+
+  const tasks = await db.task.findMany({
+    where: {
+      ownerId: user.id,
+    },
+  })
+
   return (
     <div className={'flex flex-col grow p8 mx-[16px]'}>
       <h1>Productivity App</h1>
@@ -11,12 +22,9 @@ const App = async () => {
         {/*@ts-expect-error Server Component*/}
         <UserGreetings />
       </Suspense>
-      <div className={'w-full h-full flex items-top justify-center'}><Suspense fallback={<h1>Loading tasks</h1>}>
-        <TaskList />
-      </Suspense></div>
-      <FloatingActionButton />
+      <AppClientSide tasks={tasks} />
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
