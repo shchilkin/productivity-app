7c9d34512e35276def08325f5117d978f3915e95
Aleksandr Shchilkin <crazyredkitten@icloud.com>
2023-04-03 21:32:52 +0300

    fix eslint issues

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  root: true,
+  env: {
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+    ecmaVersion: 12,
+    tsconfigRootDir: __dirname,
+  },
+  ignorePatterns: [
+    "node_modules",
+    ".eslintrc.js",
+  ],
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "prefer-const": "error",
+    "@typescript-eslint/no-unsafe-member-access": "off",
+    "@typescript-eslint/no-unused-vars": "error",
+    "@typescript-eslint/no-unsafe-call": "off",
+    "@typescript-eslint/no-unsafe-assignment": "off",
+  },
+};
