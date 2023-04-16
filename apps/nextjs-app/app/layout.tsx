8ae9253d2711f901a36be2fff636295862b431c5
Aleksandr Shchilkin <crazyredkitten@icloud.com>
2023-04-16 15:57:53 +0300

    feat(nextjs-app): add root page, update layout component

---
+++ apps/nextjs-app/app/layout.tsx
@@ -0,0 +1,11 @@
+import '@/styles/globals.css';
+import React from 'react';
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html>
+      <head />
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ apps/nextjs-app/app/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Page = () => {
+  return <h1>Productivity app</h1>;
+};
+
+export default Page;
