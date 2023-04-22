853dbdba14ff2b56c4d14ff10ec096bdd88aaede
Aleksandr Shchilkin <crazyredkitten@icloud.com>
2023-04-22 14:50:45 +0300

    fix(decorator): fix import in storybook withGlobalServices decorator

--- apps/nextjs-app/.storybook/decorators/withGlobalServices.tsx
+++ apps/nextjs-app/.storybook/decorators/withGlobalServices.tsx
@@ -2,13 +2,13 @@ import { GlobalStateContext } from '@/components/AppClientSide';
 import { useInterpret } from '@xstate/react';
 import { appMachine } from '@/actors';
 import { Decorator, StoryFn } from '@storybook/react';
-import { mockTask } from '@/utils/mock/mockTask/mockTask';
+import mockTask from '@/utils/mock/mockTask/mockTask';
 import { Task } from '@prisma/client';
 
 // create array of mock tasks
 const tasks: Task[] = [];
 for (let i = 0; i < 10; i++) {
-  tasks.push(mockTask());
+  tasks.push(mockTask({}));
 }
 const WithGlobalServices: Decorator = (Story: StoryFn) => {
   const appService = useInterpret(appMachine, {
