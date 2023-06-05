dfe6e25b7b4e0ff7014adcabbc111692a0ba459e
Aleksandr Shchilkin <crazyredkitten@icloud.com>
2023-06-05 23:00:09 +0300

    wip 2

--- apps/nextjs-app/components/common/Input/index.tsx
+++ apps/nextjs-app/components/common/Input/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
 export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+    /** Optional label that describes the input field */
+    label?: string;
+    /** Optional hint that provides more context */
+    hint?: string;
     /** Optional hint that describes the expected value of an input field */
     placeholder?: string;
 }
@@ -9,15 +13,24 @@ const focusStyles = ['focus:bg-white', 'focus:border', 'focus:border-primary', '
 
 const hoverStyles = ['hover:bg-white', 'hover:cursor-pointer', 'hover:border', 'hover:border-neutral', 'hover:outline-none', 'hover:ring-4', 'hover:ring-primary/30']
 
-const defaultStyles = ['rounded-lg', 'bg-neutral', "h-10", 'text-sm', 'pl-3', 'border-0']
+const defaultStyles = ['rounded-lg', 'bg-neutral', "h-10", 'text-sm', 'pl-3', 'border']
 
-const disabledStyles = ['disabled:opacity-50', 'disabled:cursor-not-allowed']
+const disabledStyles = ['disabled:opacity-50', 'disabled:cursor-not-allowed', 'disabled:bg-neutral', 'disabled:outline-none', 'disabled:ring-0', 'disabled:border-none']
 
-const styles = [...focusStyles, ...defaultStyles, ...hoverStyles, ...disabledStyles].join(' ');
+const invalidStyles = ['invalid:border-red-500', 'invalid:ring-red-500']
 
-console.log(styles);
+const styles = [...focusStyles, ...defaultStyles, ...hoverStyles, ...disabledStyles, ...invalidStyles].join(' ');
 
-const Input: React.FunctionComponent<InputProps> = ({...props}) => {
+const Input: React.FunctionComponent<InputProps> = ({hint, label, ...props}) => {
+
+    if (label || hint) {
+        return <div className={'flex flex-col w-full pb-4 gap-1'}>
+            {label && <label className={'font-semibold pl-1'} htmlFor={label}>{label}</label>}
+            <input className={styles} {...props} />
+            {hint && <span className={'text-xs text-gray-500 pl-1'}>{hint}</span>}
+        </div>
+    }
+
     return <input className={styles} {...props} />;
 };
 
--- apps/nextjs-app/components/common/Input/input.stories.tsx
+++ apps/nextjs-app/components/common/Input/input.stories.tsx
@@ -1,5 +1,5 @@
-import Input from './index';
 import {Meta, StoryObj} from '@storybook/react';
+import Input from './index';
 import {withDesign} from 'storybook-addon-designs';
 
 const meta: Meta = {
@@ -22,5 +22,40 @@ export const Default: Story = {
             type: 'figma',
             url: "https://www.figma.com/file/9t2wBdb5yapXuJ4TMEFgem/UI-Components?type=design&node-id=11%3A3986&t=kKmkGdBzgOd2WGL1-1"
         }
+    }
+}
+
+export const WithLabel: Story = {
+    args: {
+        label: 'Label',
+        placeholder: 'Input'
+    },
+    parameters: {
+        design: {
+            type: 'figma',
+            url: "https://www.figma.com/file/9t2wBdb5yapXuJ4TMEFgem/UI-Components?type=design&node-id=12%3A4030&t=usOSI06CU1cJFQ4t-1"
+        }
+    }
+}
+
+export const WithHint: Story = {
+    args: {
+        placeholder: 'Input',
+        hint: 'Hint which provides more context'
+    }
+}
+
+export const WithLabelAndHint: Story = {
+    args: {
+        label: 'Label',
+        placeholder: 'Input',
+        hint: 'Hint'
+    }
+}
+
+export const Disabled: Story = {
+    args: {
+        disabled: true,
+        placeholder: 'Disabled input'
     }
 }
