3ba0f8393998b8ef149c4f52ba14beb7e0733f22
Aleksandr Shchilkin <crazyredkitten@icloud.com>
2023-06-08 22:08:08 +0300

    feat(storybook): update withAuthService decorator

--- apps/nextjs-app/.storybook/decorators/withAuthServices.tsx
+++ apps/nextjs-app/.storybook/decorators/withAuthServices.tsx
@@ -4,17 +4,32 @@ import { AuthContext } from '@/components/AuthContextWrapper';
 import { useInterpret } from '@xstate/react';
 import authMachine from '@/actors/authMachine/authMachine.machine';
 
-const WithAuthServices: Decorator = Story => {
-  const authService = useInterpret(authMachine, {
-    context: {
-      mode: 'register',
-    },
-  });
-  return (
-    <AuthContext.Provider value={{ authService }}>
-      <Story />
-    </AuthContext.Provider>
-  );
-};
+const WithAuthServices =
+  (mode: 'register' | 'sign-in'): Decorator =>
+  Story => {
+    const authService = useInterpret(authMachine, {
+      context: {
+        mode,
+      },
+    });
+    return (
+      <AuthContext.Provider value={{ authService }}>
+        <Story />
+      </AuthContext.Provider>
+    );
+  };
+
+// const WithAuthServices: Decorator = (Story) => {
+//   const authService = useInterpret(authMachine, {
+//     context: {
+//       mode: 'register',
+//     },
+//   });
+//   return (
+//     <AuthContext.Provider value={{ authService }}>
+//       <Story />
+//     </AuthContext.Provider>
+//   );
+// };
 
 export default WithAuthServices;
